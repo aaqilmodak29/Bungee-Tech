04da99f30118f8ad4096119c7eff112ea834ce9c
Aaqil Modak <82268834+aaqilmodak29@users.noreply.github.com>
2021-10-21 14:18:01 +0530

    Delete Answers directory

--- Answers/answer-2/main.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import csv
-df = pd.read_csv('main.csv')
-df.head()
-df.groupby('occupation').agg({'age': ['min', 'max']})
-with open('answer1.csv', 'w') as f:
-    writer = csv.writer(f)
